8258a7cbdfe62e220e80afc75318c2f9c1ad9cde
Alireza Doostimehr <alirezadoostimehr@gmail.com>
2024-07-07 19:28:40 +0330

    Initialize commit

---
+++ cmd/serve.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start serving",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("serve called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+}
