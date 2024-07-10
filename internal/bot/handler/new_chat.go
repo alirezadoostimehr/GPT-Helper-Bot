a5e94a2bcd6773a4b8765b34897139b691678f0d
Alireza Doostimehr <alirezadoostimehr@gmail.com>
2024-07-10 23:41:36 +0330

    feat: Add conversation to new chat command

--- internal/bot/handler/new_chat.go
+++ internal/bot/handler/new_chat.go
@@ -1,8 +1,10 @@
 package handler
 
 import (
+	"context"
 	"github.com/alirezadoostimehr/GPT-Helper-Bot/internal/bot/middleware"
 	"github.com/alirezadoostimehr/GPT-Helper-Bot/internal/database"
+	"github.com/alirezadoostimehr/GPT-Helper-Bot/internal/models"
 	tb "gopkg.in/telebot.v3"
 )
 
@@ -21,10 +23,19 @@ func (n *NewChat) Command() string {
 }
 
 func (n *NewChat) Handle(ctx tb.Context) error {
-	_, err := ctx.Bot().CreateTopic(ctx.Chat(), &tb.Topic{
+	topic, err := ctx.Bot().CreateTopic(ctx.Chat(), &tb.Topic{
 		Name:            "New GPT chat",
 		IconColor:       1,
 		IconCustomEmoji: "😮‍💨",
+	})
+
+	if err != nil {
+		return err
+	}
+
+	err = n.mongoClient.CreateConversation(context.Background(), &models.Conversation{
+		ChatID:   ctx.Chat().ID,
+		ThreadID: topic.ThreadID,
 	})
 	return err
 }
